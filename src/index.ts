aecfb2182dee51dccd093795f13676147b7e5a13
Marcel Thomas <marcelthomas5@gmail.com>
2021-12-03 18:03:23 +0000

    Testing source

--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,25 @@ import { addCustomerHandler } from './stripe/add-customer';
 import { getCustomersHandler } from './stripe/get-customers';
 import { removeCustomerHandler } from './stripe/remove-customer';
 import { updateCustomerHandler } from './stripe/update-customer';
-import { getCustomers } from './stripe/utils';
 
 export const main = async (req: Request, res: Response) => {
   try {
-    const result = await getCustomers({
-      secretKey: req.body.payload.secretKey,
-    });
+    const { action } = req.body;
 
-    res.json({ result });
-  } catch (error) {
-    res.json({ error: 'Error' });
-    console.log(error);
+    switch (action) {
+      case 'getCustomers':
+        return getCustomersHandler(req, res);
+      case 'addCustomer':
+        return addCustomerHandler(req, res);
+      case 'updateCustomer':
+        return updateCustomerHandler(req, res);
+      case 'removeCustomer':
+        return removeCustomerHandler(req, res);
+      default:
+        break;
+    }
+    throw new Error(`Invalid action: ${action}`);
+  } catch (error: any) {
+    return res.status(500).json({ error: error.message || 'Failed' });
   }
-
-  // try {
-  //   const { action } = req.body;
-
-  //   switch (action) {
-  //     case 'getCustomers':
-  //       return getCustomersHandler(req, res);
-
-  //     case 'addCustomer':
-  //       return addCustomerHandler(req, res);
-
-  //     case 'updateCustomer':
-  //       return updateCustomerHandler(req, res);
-
-  //     case 'removeCustomer':
-  //       return removeCustomerHandler(req, res);
-
-  //     default:
-  //       break;
-  //   }
-
-  //   throw new Error(`Invalid action: ${action}`);
-  // } catch (error: any) {
-  //   return res.status(500).json({ error: error.message || 'Failed' });
-  // }
 };
